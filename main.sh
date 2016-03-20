2f277e86f3e17befe43fe267c5d1f6e642fa4f89
Alex Osipov <shuraosipov@gmail.com>
2016-03-20 22:50:41 +0300

    added root checker

--- main.sh
+++ main.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 # HAndy Bash system Information Tool
 
-# define functions
+
+# check root privileges
+function check_root() {
+  local user_id="$(id -u)"
+  if [[ "$user_id" -ne '0' ]]
+  then
+    echo "You must be root to start this tool"
+    exit 999
+  fi
+}
+
+
 # pauseable
 function pause() {
   local message="$@"
@@ -11,23 +22,25 @@ function pause() {
 
 # show menu options
 function show_menu() {
-    clear
-    echo "===================================="
-    echo "HABIT Main Menu"
-    echo "===================================="
-    echo "1. Show filesystem information"
-    echo "2. Show block device information"
-    echo "3. List of directories under '/' and their size"
-    echo "4. Show disk space usage"
-    echo "5. Show top memory & cpu eating process"
-    echo "6. Show network config and stats"
-    echo "7. Show operating system information"
-    echo "8. Show users activity"
-    echo "q for Exit"
-    echo ""
-    read -p "Select menu option: " answer
+  clear
+  echo "===================================="
+  echo "HABIT Main Menu"
+  echo "===================================="
+  echo "1. Show filesystem information"
+  echo "2. Show block device information"
+  echo "3. List of directories under '/' and their size"
+  echo "4. Show disk space usage"
+  echo "5. Show top memory & cpu eating process"
+  echo "6. Show network config and stats"
+  echo "7. Show operating system information"
+  echo "8. Show users activity"
+  echo "q for Exit"
+  echo ""
+  read -p "Select menu option: " answer
 }
 
+
+#check_root
 
 while true
 do
